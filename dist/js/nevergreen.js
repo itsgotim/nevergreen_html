f25ea2257c5f2ecc8d6a8d3aeed31fa2cef7b52d
Tim Schorr <39145564+itsgotim@users.noreply.github.com>
2018-10-18 18:28:07 -0800

    Update nevergreen.js

--- dist/js/nevergreen.js
+++ dist/js/nevergreen.js
@@ -1,10 +1,6 @@
 jQuery(document).ready(function() {
-		jQuery('.collapse').on('shown.bs.collapse', function () {
-			 jQuery('.navbar').addClass('navbar-solid');
-		});
-		
         //Transition effect for navbar 
-		if ( !jQuery('#navBar').hasClass('navbar-solid') ) {
+		if ( !jQuery('#navbar').hasClass('navbar-solid') ) {
 			jQuery(window).scroll(function() {
 			  //Checks if window is scrolled more than 10vh, adds/removes solid class
 			  if(jQuery(this).scrollTop() > (10 * window.innerHeight / 100) ) { //
